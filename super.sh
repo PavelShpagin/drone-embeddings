f273b57805387e660b81b6f41557d13806e874fc
PavelShpagin <pavelandrewshpagin@gmail.com>
2025-06-23 15:28:54 +0000

    adapt full GLVL

---
+++ super.sh
@@ -0,0 +1,4 @@
+python3 third_party/pytorch-superpoint/export.py export_detector_homoAdapt \
+    third_party/pytorch-superpoint/configs/magicpoint_rssdivcs_export.yaml magicpoint_synth_homoAdapt_RSSDIVCS
+
+# python3 third_party/GLVL/train.py --config third_party/GLVL/superpoint_RSSDIVCS_train.yaml
